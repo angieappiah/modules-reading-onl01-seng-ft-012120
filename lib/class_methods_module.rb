70fb268dba44189f252914746bc0ccfa917011f4
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-07 17:23:17 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -2,9 +2,5 @@ module MetaDancing
  
   def metadata
     "This class produces objects that love to dance."
-    Dancer.metadata
-// returns "This class produces objects that love to dance."
-Kid.metadata
-// returns "This class produces objects that love to dance."
   end
 end
